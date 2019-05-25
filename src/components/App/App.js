c547f182408628a24d90affd553ec2fa1983fb0a
lu.yin2046 <lu.yin2046@gmail.com>
2019-05-25 10:40:36 -0500

    finished

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,20 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import { Login } from "../Login/Login.js";
 import Home from "../Home";
-import { Switch } from "antd";
 
 class App extends Component {
-  // getHome = () => {
-  //   return this.props.isLoggedIn ? <Home /> : <Redirect to="/Login" />;
-  // };
-
-  // getLogin = () => {
-  //   return this.props.isLoggedIn ? (
-  //     <Redirect to="/home" />
-  //   ) : (
-  //     <Login handleLogin={this.props.handleLogin} />
-  //   );
-  // };
   render() {
     return (
       <BrowserRouter>
