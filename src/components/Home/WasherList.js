17238ec62670da4069c94c950ea83b262dfb93a1
lu.yin2046 <lu.yin2046@gmail.com>
2019-05-25 02:02:43 -0500

    finished

--- src/components/Home/WasherList.js
+++ src/components/Home/WasherList.js
@@ -2,7 +2,7 @@ import React from "react";
 import WasherUnit from "./WasherUnit.js";
 // import { isTSTypeAliasDeclaration } from "@babel/types";
 
-import { Button, message } from "antd";
+import { Button, message, Alert, Progress } from "antd";
 import { List, Card } from "antd";
 import { API_ROOT } from "./Constants.js";
 
@@ -19,37 +19,45 @@ class WasherList extends React.Component {
       washers: []
     };
   }
-  handleReserve = washer_id => {
-    console.log("machine id", washer_id);
-    console.log(this.state.washers);
-    fetch(
-      `${API_ROOT}/initiatetask?user_id=${user_id}&machine_id=${washer_id}`,
-      {
-        method: "POST"
-      }
-    )
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Failed");
-      })
-      .then(response => {
-        console.log("hello", response);
-        const startTime = response[0].start_time;
-        const endTime = response[0].end_time;
-        this.handleBegin(startTime, endTime);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-  handleBegin = (startTime, endTime) => {
-    const period = (Date.parse(endTime) - Date.parse(startTime)) * 0.001;
-    console.log(startTime);
-    console.log(endTime);
-    console.log("period:", period);
-  };
+
+  // handleReserve = washer_id => {
+  //   console.log("machine id", washer_id);
+  //   console.log(this.state.washers);
+  //   fetch(
+  //     `${API_ROOT}/initiatetask?user_id=${user_id}&machine_id=${washer_id}`,
+  //     {
+  //       method: "POST"
+  //     }
+  //   )
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       }
+  //       throw new Error("Failed");
+  //     })
+  //     .then(response => {
+  //       console.log("hello", response);
+
+  //       const startTime = response[0].start_time;
+  //       const endTime = response[0].end_time;
+  //       const machine_id = response[0].machine_id;
+  //       this.handleBegin(startTime, endTime, machine_id);
+  //     })
+  //     .catch(e => {
+  //       console.log(e);
+  //     });
+  // };
+  // handleBegin = (startTime, endTime, machine_id) => {
+  //   const period = (Date.parse(endTime) - Date.parse(startTime)) * 0.001;
+  //   const washer_index = Number(machine_id) - 1;
+  //   this.setState(preState => {
+  //     preState.washers[washer_index].availability = false;
+  //     const washers = preState.washers;
+  //     return {
+  //       washers
+  //     };
+  //   });
+  // };
   componentWillMount() {
     //checkavailability
     fetch(`${API_ROOT}/checkavailability`, {
@@ -64,8 +72,6 @@ class WasherList extends React.Component {
       .then(response =>
         response.map(washer => {
           this.setState(preState => {
-            // const washers = preState.washers.concat(washer);
-            // const machine_id = washer.machine_id[-1];
             const washers = preState.washers.concat(washer);
             return {
               washers
@@ -96,13 +102,8 @@ class WasherList extends React.Component {
   }
   render() {
     // console.log(this.state.washers);
-    console.log("washer:", this.state.washers[0]);
-    if (this.state.washers[0]) {
-      console.log(this.state.washers[0].machine_id);
-      var numbers = this.state.washers[0].machine_id.match(/\d+/g).map(Number);
-    }
-    // washers1[0] = 1;
-    // console.log(washers1);
+
+    const washers = this.state.washers;
 
     return (
       <div>
@@ -111,18 +112,12 @@ class WasherList extends React.Component {
         </h1>
         <div className="washers-container">
           <List
-            grid={{ gutter: 30, column: 5 }}
-            dataSource={this.state.washers}
+            grid={{ gutter: 30, column: 4 }}
+            dataSource={washers}
             renderItem={washer => (
               <List.Item>
                 <div className="washer-card">
                   <WasherUnit washer={washer} />
-                  <Button
-                    type="primary"
-                    onClick={() => this.handleReserve(washer.machine_id)}
-                  >
-                    Start
-                  </Button>
                 </div>
               </List.Item>
             )}
--- src/components/Home/WasherUnit.js
+++ src/components/Home/WasherUnit.js
@@ -1,15 +1,144 @@
 import React from "react";
 import avaiwasher from "../../images/washer1.png";
 import unavaiwasher from "../../images/washer2.png";
-import { Button, Popover } from "antd";
+import { Button, Popover, Progress, Popconfirm, message } from "antd";
+import { API_ROOT } from "./Constants.js";
 const images = [avaiwasher, unavaiwasher];
+const user_id = "1111";
 
 const imageStyle = {
   height: 100
 };
 
 class WasherUnit extends React.Component {
-  state = { washer: this.props.washer };
+  state = {
+    washer: this.props.washer,
+    loading: false,
+    timepercent: -1,
+    interval_id: null
+  };
+  confirm = e => {
+    console.log(e);
+    message.success("Pick up successfully");
+    this.setState(preState => {
+      preState.washer.availability = true;
+      const washer = preState.washer;
+      return {
+        washer: washer,
+        loading: false,
+        interval_id: null,
+        timepercent: -1
+      };
+    });
+  };
+
+  cancel = e => {
+    console.log(e);
+    message.error("Fail to pick up");
+  };
+
+  triggleProgress = () => {
+    this.startBtn = (
+      <div>
+        <Button
+          type="primary"
+          loading={this.state.loading}
+          onClick={() => this.handleReserve(this.state.washer.machine_id)}
+        >
+          Start
+        </Button>
+      </div>
+    );
+    this.progressBtn = (
+      <div>
+        <Progress percent={this.state.timepercent} />
+        <Button type="primary" loading={this.state.loading}>
+          Loading
+        </Button>
+      </div>
+    );
+    this.finishBtn = (
+      <div>
+        <Progress percent={100} />
+        <Popconfirm
+          title="Are you sure pick it up?"
+          onConfirm={this.confirm}
+          onCancel={this.cancel}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button type="primary">pickup</Button>
+        </Popconfirm>
+      </div>
+    );
+    if (this.state.timepercent == -1) {
+      return this.startBtn;
+    } else {
+      if (this.state.timepercent >= 100) {
+        clearInterval(this.state.interval_id);
+        return this.finishBtn;
+      }
+      return this.progressBtn;
+    }
+  };
+
+  handleReserve = washer_id => {
+    console.log("machine id", washer_id);
+    console.log(this.state.washers);
+    this.setState({ loading: true });
+    fetch(
+      `${API_ROOT}/initiatetask?user_id=${user_id}&machine_id=${washer_id}`,
+      {
+        method: "POST"
+      }
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Failed");
+      })
+      .then(response => {
+        console.log("hello", response);
+
+        const startTime = response[0].start_time;
+        const endTime = response[0].end_time;
+        this.handleBegin(startTime, endTime);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+  handleBegin = (startTime, endTime) => {
+    const period = (Date.parse(endTime) - Date.parse(startTime)) * 0.001;
+    const interval_id = setInterval(this.handleProgress, period * 10);
+
+    this.setState(preState => {
+      preState.washer.availability = false;
+      const washer = preState.washer;
+      return {
+        washer: washer,
+        interval_id: interval_id
+      };
+    });
+    console.log("ss", period / 100);
+  };
+  handleProgress = () => {
+    console.log("eachtime", 111);
+    this.setState(preState => ({
+      timepercent: preState.timepercent + 1
+    }));
+  };
+  switchButton = () => {
+    return (
+      <Button
+        type="primary"
+        onClick={() => this.handleReserve(this.washer.machine_id)}
+      >
+        Start
+      </Button>
+    );
+  };
 
   render() {
     const { washer } = this.state;
@@ -32,10 +161,8 @@ class WasherUnit extends React.Component {
           trigger="hover"
         >
           <img className="washer-image" style={imageStyle} src={image} />
-          {/* <Button type="primary" onClick={handleReserve}>
-            Start
-          </Button> */}
         </Popover>
+        {this.triggleProgress()}
       </div>
     );
   }
